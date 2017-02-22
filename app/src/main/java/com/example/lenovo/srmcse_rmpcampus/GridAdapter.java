885b5b48b37ebffb9f127297dac75482b874b3fd
Chirag-sam <chiraggsam@gmail.com>
2017-02-22 11:33:29 +0530

    first commit

---
+++ app/src/main/java/com/example/lenovo/srmcse_rmpcampus/GridAdapter.java
@@ -0,0 +1,76 @@
+package com.example.lenovo.srmcse_rmpcampus;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by lenovo on 2/22/2017.
+ */
+
+public class GridAdapter extends BaseAdapter {
+    private Context mContext;
+    private ArrayList<String> cate;
+    private  String[] text={
+            "Our Department",
+            "Campus life",
+            "Clubs",
+            "Placements",
+            "Academics"
+    };
+
+    private  int[] Imageid={
+            R.drawable.dept,
+            R.drawable.gallery,
+            R.drawable.club,
+            R.drawable.place,
+            R.drawable.rules
+
+
+
+    };
+    public GridAdapter(Context c) {
+        mContext = c;
+
+    }
+    @Override
+    public int getCount() {
+        return text.length;
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int i, View convertView, ViewGroup viewGroup) {
+        View v;
+
+        if (convertView == null) {
+            // if it's not recycled, initialize some attributes
+            v = LayoutInflater.from(mContext).inflate(R.layout.iconlayout,null);
+
+
+        } else {
+            v =  convertView;
+        }
+        ImageView imageView = (ImageView)v.findViewById(R.id.iv);
+        TextView textView=(TextView)v.findViewById(R.id.tv);
+        textView.setText(text[i]);
+        imageView.setImageResource(Imageid[i]);
+
+        return v;
+    }
+}
---
+++ app/src/main/java/com/example/lenovo/srmcse_rmpcampus/MainActivity.java
@@ -0,0 +1,209 @@
+package com.example.lenovo.srmcse_rmpcampus;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import static android.text.Html.fromHtml;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private MyViewPagerAdapter myViewPagerAdapter;
+    private LinearLayout dotsLayout;
+    private TextView[] dots;
+    private  int[] Imageid={
+            R.drawable.dept,
+            R.drawable.gallery,
+            R.drawable.club,
+            R.drawable.place,
+            R.drawable.rules
+
+
+
+    };
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        ViewPager viewPager = (ViewPager) findViewById(R.id.view_pager);
+
+        myViewPagerAdapter = new MyViewPagerAdapter();
+        viewPager.setAdapter(myViewPagerAdapter);
+
+
+        dotsLayout = (LinearLayout) findViewById(R.id.layoutDots);
+        addBottomDots(0);
+
+        ViewPager.OnPageChangeListener viewPagerPageChangeListener = new ViewPager.OnPageChangeListener() {
+
+            @Override
+            public void onPageSelected(int position) {
+                addBottomDots(position);
+
+
+
+            }
+
+            @Override
+            public void onPageScrolled(int arg0, float arg1, int arg2) {
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int arg0) {
+
+            }
+        };
+        viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
+
+
+
+        GridView gv=(GridView)findViewById(R.id.gridview);
+        GridAdapter ga=new GridAdapter(MainActivity.this);
+        gv.setAdapter(ga);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+    }
+    public class MyViewPagerAdapter extends PagerAdapter {
+        private LayoutInflater layoutInflater;
+
+        public MyViewPagerAdapter() {
+        }
+
+        @Override
+        public Object instantiateItem(ViewGroup container, int position) {
+            layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
+            View view = layoutInflater.inflate(R.layout.layouts, container, false);
+            ImageView iv = (ImageView)view.findViewById(R.id.iv);
+            iv.setImageResource(Imageid[position]);
+            container.addView(view);
+
+            return view;
+        }
+
+        @Override
+        public int getCount() {
+            return Imageid.length;
+        }
+
+        @Override
+        public boolean isViewFromObject(View view, Object obj) {
+            return view == obj;
+        }
+
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            View view = (View) object;
+            container.removeView(view);
+        }
+    }
+    private void addBottomDots(int currentPage) {
+        dots = new TextView[Imageid.length];
+
+        int colorsActive = getResources().getColor(R.color.dot_light_screen3);
+        int colorsInactive = getResources().getColor(R.color.dot_dark_screen3);
+
+        dotsLayout.removeAllViews();
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(fromHtml("&#8226;"));
+            dots[i].setTextSize(35);
+            dots[i].setTextColor(colorsInactive);
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0)
+            dots[currentPage].setTextColor(colorsActive);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_navigation_view) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_view) {
+
+        } else if (id == R.id.nav_view1) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
