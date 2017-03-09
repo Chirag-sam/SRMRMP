a9b880b89e62e676451c6ae42b63cd7fa39498f7
Chirag-sam <chiraggsam@gmail.com>
2017-03-09 14:34:07 +0530

    hggh \

--- app/src/main/java/com/example/lenovo/srmcse_rmpcampus/OurDepartment.java
+++ app/src/main/java/com/example/lenovo/srmcse_rmpcampus/OurDepartment.java
@@ -1,25 +1,46 @@
 package com.example.lenovo.srmcse_rmpcampus;
 
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
 import android.text.Spanned;
-import android.widget.TextView;
+import android.widget.ImageView;
+
+import com.bluejamesbond.text.DocumentView;
+import com.bumptech.glide.Glide;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class OurDepartment extends AppCompatActivity {
+
+    @BindView(R.id.tx2)
+    DocumentView tx2;
+    @BindView(R.id.iv)
+    ImageView iv;
+    @BindView(R.id.iv2)
+    ImageView iv2;
+    @BindView(R.id.tx3)
+    DocumentView tx3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_our_department);
-        TextView tv=(TextView)findViewById(R.id.textView2);
-        tv.setText(fromHtml(getString(R.string.OurDept)));
+        ButterKnife.bind(this);
+        //TextView tv = (TextView) findViewById(R.id.textView2);
+        //tv.setText(fromHtml(getString(R.string.OurDept)));
+        Glide.with(this).load("http://www.srmuniv.ac.in/ramapuram/sites/ramapuram/files/images/vision_mission.jpg").into(iv);
+        Glide.with(this).load("http://www.srmuniv.ac.in/ramapuram/sites/ramapuram/files/images/about2_hd.jpg").placeholder(R.drawable.gallery).into(iv2);
+        tx2.setText(fromHtml(getString(R.string.OurVision)));
+        tx3.setText(fromHtml(getString(R.string.OurMission)));
     }
+
     @SuppressWarnings("deprecation")
     public static Spanned fromHtml(String html) {
         Spanned result;
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
             result = Html.fromHtml(html, Html.FROM_HTML_MODE_LEGACY);
         } else {
             result = Html.fromHtml(html);
